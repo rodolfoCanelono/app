eb5749123ca2c84ecc388409e0c1ca7886e66864
rodolfoCanelono <tcanelon.osal@gmail.com>
2026-03-27 13:27:07 -0300

    Update CuentasIRM_2.py

--- CuentasIRM_2.py
+++ CuentasIRM_2.py
@@ -13,7 +13,7 @@ st.set_page_config(
 
 # Cambia la línea del engine por esta:
 # Nota: Cambiamos el puerto de 5432 a 6543
-DB_URL = "postgresql://postgres:Maniclo-2026@db.oldbexdvxquhbtpchqwe.supabase.co:5432/postgres"
+DB_URL = "postgresql://postgres:Maniclo-2026@db.oldbexdvxquhbtpchqwe.supabase.co:6543/postgres"
 engine = create_engine(
     DB_URL,
     connect_args={"sslmode": "require"} # Supabase exige SSL en conexiones externas
