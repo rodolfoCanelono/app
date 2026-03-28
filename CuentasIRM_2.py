d98b527839b654a3830fc0bb4c487b1ba3b27d11
rodolfoCanelono <tcanelon.osal@gmail.com>
2026-03-28 12:42:51 -0300

    Update CuentasIRM_2.py

--- CuentasIRM_2.py
+++ CuentasIRM_2.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, text
 import streamlit as st
 s = st.secrets["connections"]["postgresql"]
 conn_url = f"postgresql://{s['user']}:{s['password']}@{s['host']}:{s['port']}/{s['dbname']}?sslmode={s['sslmode']}"
+st.write("URL final:", repr(conn_url))
 engine = create_engine(conn_url)
 try:
     with engine.connect() as conn:
