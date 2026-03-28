38479fa098ce22a4dcb01f8cf31fc1e0df6b597f
rodolfoCanelono <tcanelon.osal@gmail.com>
2026-03-28 14:14:49 -0300

    Update CuentasIRM_2.py

--- CuentasIRM_2.py
+++ CuentasIRM_2.py
@@ -1,16 +1,6 @@
-import streamlit as st
-from sqlalchemy import create_engine, text
+import os
+from supabase import create_client, Client
 
-# String de conexión directo al pooler
-conn_url = "postgresql://postgres:Maniclo-2026@aws-0-sa-east-1.pooler.supabase.com:6543/postgres?sslmode=require"
-
-st.write("URL final limpia:", repr(conn_url))
-
-engine = create_engine(conn_url)
-
-try:
-    with engine.connect() as conn:
-        conn.execute(text("SELECT 1"))
-        st.success("✅ Conexión exitosa a Supabase")
-except Exception as e:
-    st.error(f"❌ Error de conexión: {e}")
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
