90c6addc36aaf587d66a12c8ce459d7e2c3e9c2f
rodolfoCanelono <tcanelon.osal@gmail.com>
2026-03-28 11:45:54 -0300

    Update CuentasIRM_2.py

--- CuentasIRM_2.py
+++ CuentasIRM_2.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 conn_url = st.secrets["connections"]["postgresql"]["url"]
 engine = create_engine(conn_url)
-
+st.write(repr(conn_url))
 try:
     with engine.connect() as conn:
         result = conn.execute("SELECT 1")
