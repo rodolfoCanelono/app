33ec3a1bea2d54077ea2c201c6927c25e2473ca9
rodolfoCanelono <tcanelon.osal@gmail.com>
2026-03-28 13:56:16 -0300

    Update CuentasIRM_2.py

--- CuentasIRM_2.py
+++ CuentasIRM_2.py
@@ -3,18 +3,17 @@ import socket
 from sqlalchemy import create_engine, text
 # --- Leer secretos separados ---
 s = st.secrets["connections"]["postgresql"]
-# Limpiar saltos de línea y espacios
+
 user = s['user'].replace("\n","").strip()
 password = 'Maniclo-2026'.replace("\n","").strip()
 host = s['host'].replace("\n","").strip()
 port = s['port'].replace("\n","").strip()
 dbname = s['dbname'].replace("\n","").strip()
 sslmode = s['sslmode'].replace("\n","").strip()
-"postgresql://postgres:Maniclo-2026@db.oldbexdvxquhbtpchqwe.supabase.co:5432/postgres"
 tira="postgresql://postgres:Maniclo-2026@db.oldbexdvxquhbtpchqwe.supabase.co:5432/postgres"
+
 st.write("mi tira:",repr(tira))
 conn_url = tira
-
 # --- Concatenar URL ---
 #conn_url = f"postgresql://{user}:{password}@{host}:{port}/{dbname}?sslmode={sslmode}"
 
