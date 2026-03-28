399c740becc839c62a2908f27233200e43c64a82
rodolfoCanelono <tcanelon.osal@gmail.com>
2026-03-28 13:13:24 -0300

    Update CuentasIRM_2.py

--- CuentasIRM_2.py
+++ CuentasIRM_2.py
@@ -3,15 +3,16 @@ from sqlalchemy import create_engine, text
 
 # --- Leer secretos separados ---
 s = st.secrets["connections"]["postgresql"]
-# Limpiar espacios y saltos de línea
-user = s['user'].strip()
-#password = s['password'].strip()
-password = 'Maniclo2026'.strip()
-host = s['host'].strip()
-port = s['port'].strip()
-dbname = s['dbname'].strip()
-sslmode = s['sslmode'].strip()
 
+# Limpiar saltos de línea y espacios
+user = s['user'].replace("\n","").strip()
+password = 'Maniclo2026'.replace("\n","").strip()
+host = s['host'].replace("\n","").strip()
+port = s['port'].replace("\n","").strip()
+dbname = s['dbname'].replace("\n","").strip()
+sslmode = s['sslmode'].replace("\n","").strip()
+
+# --- Concatenar URL ---
 conn_url = f"postgresql://{user}:{password}@{host}:{port}/{dbname}?sslmode={sslmode}"
 st.write("URL final limpia:", repr(conn_url))  # Para debug
 
