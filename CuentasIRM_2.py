f55bc3ee34a1957afc9167bf15cb7b7e2031276e
rodolfoCanelono <tcanelon.osal@gmail.com>
2026-03-28 12:01:52 -0300

    Update CuentasIRM_2.py

--- CuentasIRM_2.py
+++ CuentasIRM_2.py
@@ -1,12 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import streamlit as st
+
 conn_url = st.secrets["connections"]["postgresql"]["url"]
 conn_url = conn_url.replace("\n", "").replace("\r", "").strip()
+
+st.write("URL limpia:", repr(conn_url))
+
 engine = create_engine(conn_url)
-st.write(repr(conn_url))
+
 try:
     with engine.connect() as conn:
-        result = conn.execute("SELECT 1")
+        conn.execute(text("SELECT 1"))
         st.success("✅ Conexión exitosa")
 except Exception as e:
     st.error(f"Error de conexión: {e}")
